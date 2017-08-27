de4809f56bee52b4af5064dd46e59be8dcee761b
mint <3432747@mail.ru>
2017-08-27 13:52:09 +0300

    SecureImput-function added. Cyclic BinSearch added.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <cctype>
+#include <cstdio>
 #include <assert.h>
+
+#define MAX_INPUT_SIZE 10
 
 using namespace std;
 
 int  SecureUserInput();
-int* InsertBinSort(int* arr_to_sort, int elements_amnt);
-int  BinSearch(int* arr_sorted, int start_pos, int end_pos, int curr_elem);
-int* FillArray(int* arr_to_fill, int elements_amnt, int max_elem_value = 100);
-void PrintArray(int* arr_to_print, int elements_amnt);
+int* InsertBinSort(int* arr_to_sort, size_t elements_amnt);
+int  BinSearchR(int* arr_sorted, int start_pos, int end_pos, int curr_elem);
+int  BinSearchC(int* arr_sorted, int start_pos, int end_pos, int curr_elem);
+int* FillArray(int* arr_to_fill, size_t elements_amnt, int max_elem_value = 100);
+void PrintArray(int* arr_to_print, size_t elements_amnt);
 void ElemInsert(int* arr_to_ins_in, int start_pos, int end_pos,
                 int new_elem_pos, int new_elem_value);
 
@@ -18,7 +21,9 @@ int main()
     cout << "# \"InsertSort + BinSearch\" - function demonstration." << endl;
     cout << "# Me, 08.2017\n" << endl;
 
-    cout << "Enter amount of elements in array (this number should be > 0): " << endl;
+    cout << "Enter amount of elements in array (this number should be > 0)." << endl;
+    cout << "Do not forget, that your number will be interpreted as integer." << endl;
+    cout << "Enter size: ";
     int elements_amnt = SecureUserInput();
 
     int* arr = new int [elements_amnt];
@@ -38,21 +43,21 @@ int main()
 
 int  SecureUserInput()
 {
-    int elements_amnt;
+    int elements_amnt = 0;
 
-    bool imput_correct = false;
-    while(!imput_correct){
-        cin >> elements_amnt;
+    while(elements_amnt < 1){
+        char input[MAX_INPUT_SIZE] = "";
+        scanf("%s", input);
+        sscanf(input, "%d", &elements_amnt);
 
         if(elements_amnt <= 0)
-            cout << "Wrong input. Number should be more then 0." << endl;
-        else
-            imput_correct = true;
+            cout << "Wrong input." << endl;
     }
+
     return elements_amnt;
 }
 
-int* InsertBinSort(int* arr_to_sort, int elements_amnt)
+int* InsertBinSort(int* arr_to_sort, size_t elements_amnt)
 {
     //Exceptions
     assert(arr_to_sort != nullptr);
@@ -65,7 +70,7 @@ int* InsertBinSort(int* arr_to_sort, int elements_amnt)
     int current_pos = 1;
     while(current_pos < elements_amnt){
         int current_elem_value = arr_to_sort[current_pos];
-        int new_pos = BinSearch(arr_to_sort, 0, current_pos, current_elem_value);
+        int new_pos = BinSearchC(arr_to_sort, 0, current_pos, current_elem_value);
 
         ElemInsert(arr_to_sort, 0, current_pos, new_pos, current_elem_value);
 
@@ -76,7 +81,7 @@ int* InsertBinSort(int* arr_to_sort, int elements_amnt)
     return arr_to_sort;
 }
 
-int  BinSearch(int* arr_sorted, int start_pos, int end_pos, int curr_elem)
+int  BinSearchR(int* arr_sorted, int start_pos, int end_pos, int curr_elem)
 {
     //Exceptions
     assert(arr_sorted != nullptr);
@@ -89,16 +94,35 @@ int  BinSearch(int* arr_sorted, int start_pos, int end_pos, int curr_elem)
 
         int new_pos;
         if(curr_elem < arr_sorted[middle_pos])
-            new_pos = BinSearch(arr_sorted, start_pos, middle_pos, curr_elem);
+            new_pos = BinSearchR(arr_sorted, start_pos, middle_pos, curr_elem);
         else
-            new_pos = BinSearch(arr_sorted, middle_pos + 1, end_pos, curr_elem);
+            new_pos = BinSearchR(arr_sorted, middle_pos + 1, end_pos, curr_elem);
 
         return new_pos;
     }else /*if (end_pos <= start_pos)*/
         return start_pos;
 }
 
-int* FillArray(int* arr_to_fill, int elements_amnt, int max_elem_value)
+int  BinSearchC(int* arr_sorted, int start_pos, int end_pos, int curr_elem)
+{
+    //Exceptions
+    assert(arr_sorted != nullptr);
+    assert(end_pos >= start_pos);
+    assert(start_pos >= 0);
+    assert(end_pos >= 0);
+
+    while(start_pos != end_pos){
+        int middle_pos = (start_pos + end_pos)/2;
+
+        if(curr_elem < arr_sorted[middle_pos])
+            end_pos = middle_pos;
+        else
+            start_pos = middle_pos + 1;
+    }
+    return start_pos;
+}
+
+int* FillArray(int* arr_to_fill, size_t elements_amnt, int max_elem_value)
 {
     //Exceptions
     assert(arr_to_fill != nullptr);
@@ -110,7 +134,7 @@ int* FillArray(int* arr_to_fill, int elements_amnt, int max_elem_value)
     return arr_to_fill;
 }
 
-void PrintArray(int* arr_to_print, int elements_amnt)
+void PrintArray(int* arr_to_print, size_t elements_amnt)
 {
     //Exceptions
     assert(arr_to_print!= nullptr);
